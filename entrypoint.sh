f26508c149978f5b443e9638a980f848c3788344
daelynum <vivanov89@gmail.com>
2023-07-31 00:38:14 +0400

    first commit (test 7)

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,14 +63,16 @@ replace_variables () {
 
    # Display the file being processed
    echo "Processing $FILE_PATH"
+
+   # Loop through the files in the REPLACEMENT_DIRECTORY
    for FILE_PATH in $(find "$REPLACEMENT_DIRECTORY" -type f); do
-     while IFS= read -r line
-     do
-        REPLACEMENT_VARIABLE=$(echo "$line" | awk -F= '{print $1}')
-        REPLACEMENT_VALUE=$(echo "$line" | awk -F= '{print $2}')
-        echo $line
-        sed -i -e 's/{{'"$REPLACEMENT_VARIABLE"'}}/'"$REPLACEMENT_VALUE"'/g' "$FILE_PATH"
-     done < "$REPLACEMENT_DIRECTORY"
+      # Read the replacement variables from the specified file
+      while IFS= read -r line
+      do
+         REPLACEMENT_VARIABLE=$(echo "$line" | awk -F= '{print $1}')
+         REPLACEMENT_VALUE=$(echo "$line" | awk -F= '{print $2}')
+         sed -i -e 's/{{'"$REPLACEMENT_VARIABLE"'}}/'"$REPLACEMENT_VALUE"'/g' "$FILE_PATH"
+      done < "$FILE_PATH"
    done
 }
 
